cdf2fd3430da448b2ca69bbceb9f3645a8ebafd2
Villuntos-MacMini <victorctmgeo@gmail.com>
2024-11-28 16:46:21 -0300

    :ok_hand: main ok

--- src/main/java/br/com/skafikis/Main.java
+++ src/main/java/br/com/skafikis/Main.java
@@ -1,7 +1,5 @@
 package br.com.skafikis;
 
-import br.com.skafikis.infra.console.ConsoleUI;
-import br.com.skafikis.infra.testdb.UserTestDBRepository;
 import br.com.skafikis.service.Application;
 
 import java.io.Console;
@@ -10,10 +8,6 @@ public class Main {
     public static void main(String[] args) {
 
         Application app = new Application();
-
-        ConsoleUI consoleUI = new ConsoleUI();
-        UserTestDBRepository userTestDBRepository = new UserTestDBRepository();
-
-        app.execute(consoleUI, userTestDBRepository);
+        app.execute();
     }
 }
