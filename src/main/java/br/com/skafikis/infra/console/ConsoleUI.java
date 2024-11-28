ac8a496a34b7f554b3c8116585696bc727f8fec8
villuntos-skafikis <prawn.staging-0x@icloud.com>
2024-11-28 04:20:19 -0300

    :ok_hand: i am on my way  commit

--- src/main/java/br/com/skafikis/infra/console/ConsoleUI.java
+++ src/main/java/br/com/skafikis/infra/console/ConsoleUI.java
@@ -32,26 +32,9 @@ public class ConsoleUI implements IUserInterface {
     }
 
     @Override
-    public Integer showHomeMenu(String document) {
-        Integer menu = null;
-
-        while (menu == null) {
-            try {
-                System.out.println("Hello, " + document + " !");
-                System.out.println("[1] Generate QR Code Pix");
-                System.out.println("[2] Consult Bill");
-                System.out.println("[3] Make Bill Payment");
-
-                menu = Integer.parseInt(scanner.nextLine());
-
-            } catch (Exception exception) {
-                System.out.println("Invalid input. Please enter a number.");
-            }
-        }
-
-        return menu;
+    public Integer showHomeMenu() {
+        return 0;
     }
-
 
     @Override
     public LoginDTO getLoginData() {
@@ -66,25 +49,7 @@ public class ConsoleUI implements IUserInterface {
 
     @Override
     public UserAccountDTO getUserData() {
-        System.out.println("Enter Your Name:");
-        String name = scanner.nextLine();
-
-        System.out.println("Enter Your Email:");
-        String email = scanner.nextLine();
-
-        System.out.println("Enter Your Document:");
-        String document = scanner.nextLine();
-
-        System.out.println("Enter Your Account Number:");
-        String number = scanner.nextLine();
-
-        System.out.println("Enter Your Created At:");
-        String createAt = scanner.nextLine();
-
-        System.out.println("Enter Your Disabled At:");
-        String disabledAt = scanner.nextLine();
-
-        return new UserAccountDTO(null, String email, String document);
+        return null;
     }
 
     @Override
