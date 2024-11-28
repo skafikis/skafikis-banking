756d9acf23f7867f28419d2176fa30d609897547
Villuntos-MacMini <victorctmgeo@gmail.com>
2024-11-28 16:47:02 -0300

    :bug: warn (swing)

--- src/main/java/br/com/skafikis/service/Application.java
+++ src/main/java/br/com/skafikis/service/Application.java
@@ -3,8 +3,42 @@ package br.com.skafikis.service;
 import br.com.skafikis.domain.repositories.IUserInterface;
 import br.com.skafikis.domain.repositories.IUserRepository;
 import br.com.skafikis.dto.UserAccountDTO;
+import br.com.skafikis.infra.console.ConsoleUI;
+import br.com.skafikis.infra.swing.SwingUI;
+import br.com.skafikis.infra.testdb.UserTestDBRepository;
+
+import java.util.Scanner;
 
 public class Application {
+
+    public void execute(){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Choose Interface: ");
+        System.out.println("[1] Console");
+        System.out.println("[2] Swing");
+
+        Integer choice = null;
+
+        while (choice == null || (choice != 1 && choice != 2)) {
+
+                System.out.println("Enter choice: ");
+                choice = scanner.nextInt();
+
+                if (choice != 1 && choice != 2) {
+                    System.out.println("Invalid choice. Please enter 1 or 2!");
+                }
+        }
+
+        IUserInterface userInterface = (choice == 1) ? new ConsoleUI() : new SwingUI();
+        IUserRepository userRepository = new UserTestDBRepository();
+
+        //TODO: Need configuration to Supabase
+        // Here!
+
+        execute(userInterface, userRepository);
+    }
 
     public void execute(IUserInterface userInterface, IUserRepository userRepository) {
 
@@ -28,15 +62,12 @@ public class Application {
                     if (createNewAccount != null) {
                         bankingService.execute(createNewAccount);
                     }
-
                     break;
                 case 3:
                     userInterface.showExitMessage();
-
                     return;
                 default:
                     System.out.println("Invalid option");
-
                     break;
             }
         }
